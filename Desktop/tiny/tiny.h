d74c9259170553e813c5e5529837ac0f8d180399
nmakes <nav.naveenvenkat@gmail.com>
2016-06-06 22:50:59 +0530

    Update tiny.h

--- Desktop/tiny/tiny.h
+++ Desktop/tiny/tiny.h
@@ -4,9 +4,9 @@
 
 	Warning: The following content is licensed under a Creative Commons 
 	Attribution-NonCommercial-NoDerivatives 4.0 International License. No part
-	of the product is in the public domain.
+	of the product is in the public domain and may not be used without attribution.
 
-	If you don't agree with the	terms in EULA, destroy any copy of the software 
+	If you don't agree with the terms in EULA, destroy any copy of the software 
 	immediately.
 
 	Visit serverprocessor.wordpress.com/tiny to learn more.
