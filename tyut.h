8e17c8f9bd4a284c1ee6facc355ab82ee29ecc64
Naveen Venkat <nav.naveenvenkat@gmail.com>
2017-01-30 18:26:29 +0000

    added proper folder

---
+++ tyut.h
@@ -0,0 +1,22 @@
+/*
+	Tiny Language
+	Copyright (c) 2016 Naveen Venkat | All Rights Reserved
+
+	Warning: The following content is licensed under a Creative Commons 
+	Attribution-NonCommercial-NoDerivatives 4.0 International License. No part
+	of the product is in the public domain and may not be used without attribution.
+
+	If you don't agree with the terms in EULA, destroy any copy of the software 
+	immediately.
+
+	Visit serverprocessor.wordpress.com/tiny to learn more.
+*/
+
+#ifndef __UTILS_H
+#define __UTILS_H
+#endif
+
+#ifndef __TINY_H
+#include "tiny.h"
+#endif
+
